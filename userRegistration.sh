9c02d17a2e1145a51d632934ace363a3bc9b9eff
NitinJ1996 <nitin1996j@gmail.com>
2020-07-15 11:24:55 +0530

    {Nitin} UseCase9 - Code refactoring Update

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,51 +1,112 @@
 #!/bin/bash -x
 shopt -s extglob
 
-read -p "Enter the first name: " fname
+patternfname="^[[:upper:]]{1}[[:lower:]]{2,}$"
+patternlname="^[[:upper:]][[:lower:]]{2,}$"
+patternemail="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-z]+\.[a-z]{2,}([.]{1}[a-z]{2,}){0,2}$"
+patternmobile="^[0-9]{2}[[:space:]][1-9][0-9]{9}$"
+count=0
 
-if [[ $fname =~ ^[[:upper:]]{1}[[:lower:]]{2,}$ ]]
-then
-	echo "FirstName is valid"
-else
-	echo "Enter a valid name with first letter as Capital"
-fi
+function fname_validation(){
+valid=true
+        while [ $valid ]
+        do
+	read -p "Enter the first name: " fname
 
-read -p "Enter the last name: " lname
+	if [[ $fname =~ $patternfname ]]
+	then
+		echo "FirstName is valid"
+		valid=false
+		((count++))
+		break
+	else
+		echo "Enter a valid name with first letter as Capital"
+	fi
+	done
+}
 
-if [[ $lname =~ ^[[:upper:]][[:lower:]]{2,} ]]
-then
-        echo "LastName is valid"
-else
-        echo "Enter a valid last name with First letter as Capital"
-fi
+function lname_validation(){
+        valid=true
+        while [ $valid ]
+        do
 
-read -p "Enter an email: " email
+	read -p "Enter the last name: " lname
 
-pat="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-z]+\.[a-z]{2,}([.]{1}[a-z]{2,}){0,2}$"
+	if [[ $lname =~ $patterlname ]]
+	then
+        	echo "LastName is valid"
+	valid=false
+                ((count++))
+                break
+	else
+        	echo "Enter a valid last name with First letter as Capital"
+	fi
+	done
+}
 
-if [[ $email =~ $pat ]]
-then
-        echo "Email is Valid"
-else
+function email_validation(){
+        valid=true
+        while [ $valid ]
+        do
+	read -p "Enter an email: " email
+	if [[ $email =~ $patternemail ]]
+	then
+        	echo "Email is Valid"
+		valid=false
+		((count++))
+		break
+	else
         echo "Enter a valid Email"
-fi
+	fi
+	done
+}
 
-read -p "Enter a Mobile number: " number
+function ph_num_validation(){
+        valid=true
+        while [ $valid ]
+        do
 
-if [[ $number =~ ^[0-9]{2}[[:space:]][1-9][0-9]{9}$ ]]
+	read -p "Enter a Mobile number: " number
+
+	if [[ $number =~ $patternmobile ]]
+	then
+        	echo "Number valid"
+		valid=false
+		((count++))
+		break
+	else
+        	echo "Enter a valid number"
+	fi
+	done
+}
+
+function password_validation(){
+        special='^[A-Za-z0-9]*[@#$%&=*_-][A-Za-z0-9]*$'
+        valid=true
+        while [ $valid ]
+        do
+	read -p "Enter a password: " password
+
+	if [[ ${#password} -ge 8 && "$password" =~ [[:upper:]]+ && "$password" =~ [[:digit:]]+ && $password =~ $special ]]
+	then
+        	echo "Password Valid"
+		valid=false
+		((count++))
+		break
+	else
+        	echo "Password Not Valid"
+	fi
+	done
+|
+
+fname_validation
+lname_validation
+email_validation
+ph_num_validation
+password_validation
+
+if [ $count -eq 5 ]
 then
-        echo "Number valid"
-else
-        echo "Enter a valid number"
-fi
-
-read -p "Enter a password: " password
-
-special='^[A-Za-z0-9]*[@#$%&=*_-][A-Za-z0-9]*$'
-if [[ ${#password} -ge 8 && "$password" =~ [[:upper:]]+ && "$password" =~ [[:digit:]]+ && $password =~ $special ]]
-then
-        echo "Password Valid"
-else
-        echo "Password Not Valid"
+        echo "Registration Successful"
 fi
 
