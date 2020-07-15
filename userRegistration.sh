3efef58148ffddfb4f8acf4ecd82dc5013181afd
NitinJ1996 <nitin1996j@gmail.com>
2020-07-15 09:37:52 +0530

    {Nitin} Code Refactoring - email section

--- userRegistration.sh
+++ userRegistration.sh
@@ -21,7 +21,7 @@ fi
 
 read -p "Enter an email: " email
 
-pat="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-z]+\.[a-z]{2,}([.]{1}[a-z]{2,})"
+pat="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-z]+\.[a-z]{2,}([.]{1}[a-z]{2,}){0,2}$"
 
 if [[ $email =~ $pat ]]
 then
